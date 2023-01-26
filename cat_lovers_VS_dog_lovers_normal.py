178d5401e02a4089d8a7b72ec33ef77a8cb5ef1f
Prajwal T <tkushal216@gmail.com>
2023-01-25 17:37:53 -0700

    updated the code base

--- cat_lovers_VS_dog_lovers_normal.py
+++ cat_lovers_VS_dog_lovers_normal.py
@@ -10,8 +10,6 @@ import pyfiglet
 from termcolor import colored
 from colorama import Style, Fore
 from time import time
-import time
-import logging
 
 # Banner to display the title
 title_name = "Cat <3 VS Dog <3"
@@ -37,81 +35,48 @@ vote_count_2 = fhel.encryptInt(integer0)
 elapsed_time_1 = list() 
 elapsed_time_2 = list()
 
-# Configuring logging
-logging.basicConfig(filename='voting.log', encoding='utf-8', level=logging.DEBUG)
-
-
 def cast_vote(candidate_number):
     if candidate_number == 1:
         global vote_count_1
         global elapsed_time_1
 
         # starting the timer to capture the execution time for this operation
-        start_time = time.time()
+        start_time = time()
         vote_count_1 = fhel.add(vote_count_1, fhel.encryptInt(integer1))
-        end_time = time.time()
+        end_time = time()
         
-        logging.info(vote_count_1)
+        print(f"Encrypted vote_count_1 {vote_count_1}")
         elapsed_time_1.append((end_time - start_time))
-        elapsed_time_now = end_time - start_time
-        print(f"Time taken to perform 1 addition in homomorphic operation {elapsed_time_now}\n")
+        print(f"Time to perform 1 addition in homomorphic operation {elapsed_time_1}")
 
     elif candidate_number == 2:
         global vote_count_2
         global elapsed_time_2
 
         # starting the timer to capture the execution time for this opertion 
-        start_time = time.time()
+        start_time = time()
         vote_count_2 = fhel.add(vote_count_2, fhel.encryptInt(integer1))
-        end_time = time.time()
+        end_time = time()
 
-        logging.info(vote_count_2)
+        print(f"Encrypted vote_count_2 {vote_count_2}") 
         elapsed_time_2.append(( end_time - start_time ))
-        elapsed_time_now = end_time - start_time
-        print(f"Time taken to perform 1 addition in homomorphic operation {elapsed_time_now}\n")
+        print(f"Time to perform 1 addition in homomorphic operation {elapsed_time_2}")
 
     else:
-        print("Wrong candidate number.You are a HATER lol jk")
+        print("Wrong candidate number")
 
 # Function to decrypt and display vote counts
 def display_results():
     #cat = emoji.emojize(":thumbsup")
-
     cat = "\U0001F63B"
     heart = "\U00002764"
     dog = "\U0001F415"
     elapsed_time = sum(elapsed_time_1) + sum(elapsed_time_2)
-    
-    print("\r")
-    #print("\tRESULTS")
-    results = "PLEASE WAIT, VOTES ARE BEING COUNTED!! HOMOMORPHIC ENCRYPTION IS DOING ITS MAGIC \n"
-    for char in results:
-        print(char, end="", flush=True)
-        time.sleep(0.1)
-    print("VOTING RESULTS")
-    print("*" * 50 )
     print(f"CAT Lovers{cat} {heart}: {fhel.decryptInt(vote_count_1)[0]}")
     print(f"DOG Lovers{dog} {heart}: {fhel.decryptInt(vote_count_2)[0]}")
-    print("*" * 50)
-    
-    # Announce the winner
-    # cat lovers winning 
-    cat_votes = fhel.decryptInt(vote_count_1)[0]
-    dog_votes = fhel.decryptInt(vote_count_2)[0]
-    if cat_votes > dog_votes:
-        print(f"WINNERS are CAT LOVERS {cat} {heart}")
-    elif cat_votes < dog_votes:
-        print(f"WINNERS are DOG LOVERS {dog} {heart}")
-    else:
-        print(f"BOTH ARE WINNERS {cat} {heart} {dog}")
+    print(f"Time taken to compute each votes {elapsed_time_1} {elapsed_time_2}")
+    print(f"Total time {elapsed_time}")
 
-    print("\n")
-    print("EXECUTION TIME")
-    print("#" * 25)
-    #print(f"Time taken to compute each votes {elapsed_time_1} {elapsed_time_2}")
-    print(f"Total time taken to compute {elapsed_time}")
-    
-    print(f"\nFOR MORE INFORMATION, PLEASE CHECK voting.log FOR LOGS")
 
 def get_input():
     vote = int(input("CAT Lovers press 1, DOG Lovers press 2: \t"))
@@ -119,7 +84,6 @@ def get_input():
 
 
 n = int(input("How many people are voting today? \n"))
-print("\r")
 for x in range(0,n):
     get_input()
 
